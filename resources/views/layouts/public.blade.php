b1ca8651db0ced7889ac38c23f294061379965f0
Sistemas Cerízeon <sistemascerizeon@gmail.com>
2020-07-15 18:09:29 -0400

    algunos cambios

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -91,7 +91,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 					<br>
 					<span class="">Correo:</span>
 					<br>
-                    <span class=""><a href="mailto:contacto@bosquessur.cl">contacto@bosquessur.cl</a></span>
+                    <span class=""><a href="mailto:contacto@bosquessur.cl">bosquessur@gmail.com</a></span>
 					<br/>
 					<br>
 					<span class="">Teléfonos:</span>
